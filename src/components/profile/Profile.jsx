dccc1d8f47bd8083b9a3b0ed0e52dc625972dd44
Izak Filmalter <izak@whoatech.com>
2018-03-20 09:01:36 -0400

    Schafold out basic fetch from dog.ceo api.

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,9 +1,44 @@
 // Import React
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      dogImage: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            dogImage: result.message,
+          });
+        },
+
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
+
   render() {
-    return <div>profile</div>
+    const { error, isLoaded, dogImage } = this.state;
+
+    return <div>profile</div>;
   }
 }
 
